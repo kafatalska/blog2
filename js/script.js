a624b5f3acbc09d3588f47fdfceab16ceeaef1d8
Katarzyna Fatalska <kafatalska@gmail.com>
2022-03-17 12:38:03 +0100

    Add code to function titleClickHandler in js

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ const titleClickHandler = function(event){
   console.log('clickedElement:', clickedElement);
   
   /* [DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('posts.active');
+  const activeArticles = document.querySelectorAll('.post.active');
 
   for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
